53e4b9020420887e216bb60aaf14162de8a028cc
Ali Gasimzade <gasimzadeali@gmail.com>
2020-03-08 16:15:19 +0400

    Updated the Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
@@ -6,7 +8,7 @@ class SearchBar extends React.Component {
     render () {
         return (
             <div className="searchBarUnFocused">
-                Search
+                <FontAwesomeIcon icon={faSearch} style={{fontSize: '10pt'}} /> Search
             </div>
         )
     }
